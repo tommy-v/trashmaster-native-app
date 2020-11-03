5c3ea5c8ed37784d85556398b61488f35e809891
Thomas Vandercam <thomasvandercam@Thomass-MacBook-Pro.local>
2020-11-02 22:33:22 -0500

    feat(map): design improvements, clean code and refactor

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,12 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, View, Dimensions, TouchableOpacity} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
 
 // Redux
 import { useDispatch, useSelector } from 'react-redux';
-import {getAllTrashes} from '../redux/actions/trash';
+import { getAllTrashes } from '../redux/actions/trash';
+
+// Components
+import { StyleSheet, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
-import AddButton from '../components/AddButton'; 
+import AddButton from '../components/AddButton';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const screen = Dimensions.get('window');
 
@@ -17,47 +20,51 @@ const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 interface Props {
   navigation: any;
 }
+interface State {
+  location: any;
+}
 
-export default (props: any) => {
-  const [state, setState] = useState({
+export default (props: Props) => {
+  const [state, setState] = useState<State>({
     location: null,
   });
   const dispatch = useDispatch();
-  const trashState = useSelector(state => state.trash);
+  const trashState = useSelector((state: any) => state.trash);
 
   useEffect(() => {
     dispatch(getAllTrashes());
 
     Geolocation.getCurrentPosition(
-      info => {
-        setState(state => {
+      (info: any) => {
+        setState((state: any) => {
           return {
             ...state,
             location: info.coords,
-          }
-        })
+          };
+        });
         location = info;
       },
-      error => {
+      (error: any) => {
         console.log('Error:', error);
       },
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
     );
-
   }, [dispatch, setState]);
 
   const goToPersonalLocation = () => {
-    mainMap.animateCamera({
-      center: {
-        latitude: state.location.latitude,
+    mainMap.animateCamera(
+      {
+        center: {
+          latitude: state.location.latitude,
           longitude: state.location.longitude,
-      } },
-      {duration: 1000}
+        },
+      },
+      { duration: 1000 },
     );
-  }
+  };
 
   const createMarkers = () => {
-    return trashState.mapTrashes.map(trash => (
+    return trashState.mapTrashes.map((trash: any) => (
       <Marker.Animated
         key="ADD_KEY"
         identifier={trash.id}
@@ -67,31 +74,30 @@ export default (props: any) => {
           latitudeDelta: LATITUDE_DELTA,
           longitudeDelta: LONGITUDE_DELTA,
         }}
-        onPress={$event => trashDetails($event)}
+        onPress={($event: any) => trashDetails($event)}
         image={require('../assets/marker-3.png')}
-        centerOffset={{x: 0, y: -50}}
+        centerOffset={{ x: 0, y: -50 }}
       />
     ));
-  }
+  };
 
-  const trashDetails = $event => {
+  const trashDetails = ($event: any) => {
     console.log('id trash', $event.nativeEvent.id);
     props.navigation.navigate('trashDetails', {
-      trashId: $event.nativeEvent.id
+      trashId: $event.nativeEvent.id,
     });
-  }
+  };
 
   const openCamera = () => {
     props.navigation.navigate('camera');
-  }
+  };
 
   return (
     <View style={styles.container}>
-      {
-        state.location &&
+      {state.location && (
         <MapView
           style={styles.map}
-          ref={ref => {
+          ref={(ref: any) => {
             mainMap = ref;
           }}
           initialRegion={{
@@ -100,23 +106,20 @@ export default (props: any) => {
             latitudeDelta: LATITUDE_DELTA,
             longitudeDelta: LONGITUDE_DELTA,
           }}
-          showsUserLocation={true}
-          // followsUserLocation={true}
-          >
+          showsUserLocation={true}>
           {createMarkers()}
         </MapView>
-      }
-      
+      )}
 
       <View style={styles.buttonContainer}>
-        <View style={styles.littleButtonsContainer}>
-          <View />
-          <AddButton action={openCamera} />
-          <TouchableOpacity
-            onPress={() => goToPersonalLocation()}
-            style={[styles.locationButton]}
-          />
-        </View>
+        <View style={styles.leftButton} />
+        <AddButton action={openCamera} />
+        <TouchableOpacity
+          onPress={() => goToPersonalLocation()}
+          style={styles.locationButton}>
+          <Icon name="location-arrow" size={25} color="rgba(255,255,255,0.7)" />
+        </TouchableOpacity>
+        {/* </View> */}
       </View>
     </View>
   );
@@ -137,28 +140,20 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
   },
   locationButton: {
+    right: 20,
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  leftButton: {
     width: 20,
     height: 20,
     borderRadius: 40,
     borderWidth: 3,
-    right: 20,
-    borderColor: 'rgba(255,255,255,0.8)',
-    backgroundColor: 'rgba(255,255,255,0.5)',
-  },
-  trashButton: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-    borderWidth: 6,
-    borderColor: 'rgba(255,255,255,0.8)',
-    backgroundColor: 'rgba(255,255,255,0.5)',
-    marginBottom: 40,
-  },
-  littleButtonsContainer: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-  },
-  buttonContainer: {
-    display: 'flex',
+    left: 20,
+    borderColor: 'rgba(255,255,255,0)',
+    backgroundColor: 'rgba(255,255,255,0)',
   },
 });
